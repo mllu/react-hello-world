7bc6275a42c088a503b3f6e88c97943c9ef9348e
mllu <mornlyn@gmail.com>
2017-03-14 07:29:39 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+
+Reference:
+[A Beginner's Guide to Setup React.js Environment Using Npm, Babel 6 and Webpack](http://blog.tamizhvendan.in/blog/2015/11/23/a-beginner-guide-to-setup-react-dot-js-environment-using-babel-6-and-webpack/)
+
+Instructions:
+1. npm install
+2. npm run dev
+
---
+++ src/client/app/index.jsx
@@ -0,0 +1,28 @@
+console.log('Hello World!');
+
+import React from 'react';
+import {render} from 'react-dom';
+
+import AwesomeComponent from './AwesomeComponent.jsx';
+
+class App extends React.Component {
+  render() {
+    return (
+        <div>
+          <p> Hello React!</p>
+          <AwesomeComponent />
+        </div>
+    );
+  }
+}
+
+/*
+class App extends React.Component {
+  render () {
+    return <p> Hello React!</p>;
+  }
+}
+*/
+
+render(<App/>, document.getElementById('app'));
+
